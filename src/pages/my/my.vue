6a6287f6a14e62146f2efc2a5a09e7ec60bbd76f
zhiyuezz <3032149617@qq.com>
2023-11-09 19:27:52 +0800

    new file:   .editorconfig 	new file:   .eslintignore 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   .husky/pre-commit 	new file:   .prettierignore 	new file:   .prettierrc.cjs 	new file:   .vscode/extensions.json 	new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   NLGL-shop.code-workspace 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   pnpm-lock.yaml 	new file:   src/App.vue 	new file:   src/api/.gitkeep 	new file:   src/api/login.ts 	new file:   src/components/.gitkeep 	new file:   src/env.d.ts 	new file:   src/hooks/.gitkeep 	new file:   src/main.ts 	new file:   src/manifest.json 	new file:   src/pages.json 	new file:   src/pages/cart/cart.vue 	new file:   src/pages/category/category.vue 	new file:   src/pages/index/index.vue 	new file:   src/pages/login/login.vue 	new file:   src/pages/my/my.vue 	new file:   src/static/images/blank.png 	new file:   src/static/images/blank_cart.png 	new file:   src/static/images/bubble.png 	new file:   src/static/images/car.png 	new file:   src/static/images/center_bg.png 	new file:   src/static/images/default-a.png 	new file:   src/static/images/locate.png 	new file:   src/static/images/logo.png 	new file:   src/static/images/navigator_bg.png 	new file:   src/static/images/order_bg.png 	new file:   src/static/images/rating_off.png 	new file:   src/static/images/rating_on.png 	new file:   src/static/images/remove.png 	new file:   src/static/images/stars.png 	new file:   src/static/tabs/cart_default.png 	new file:   src/static/tabs/cart_selected.png 	new file:   src/static/tabs/category_default.png 	new file:   src/static/tabs/category_selected.png 	new file:   src/static/tabs/home_default.png 	new file:   src/static/tabs/home_selected.png 	new file:   src/static/tabs/user_default.png 	new file:   src/static/tabs/user_selected.png 	new file:   src/stores/index.ts 	new file:   src/stores/modules/member.ts 	new file:   src/styles/fonts.scss 	new file:   src/styles/global.scss 	new file:   src/types/component.d.ts 	new file:   src/types/params.d.ts 	new file:   src/uni.scss 	new file:   src/utils/element-china-area-data.mjs 	new file:   src/utils/http.ts 	new file:   tsconfig.json 	new file:   vite.config.ts 	new file:   windi.config.js

---
+++ src/pages/my/my.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+</script>
+
+<template>
+  <view class="my">
+    <view>用户信息：{{ memberStore.profile }}</view>
+    <button @tap="memberStore.setProfile({ nickname: '王東' })" size="mini" plain type="primary">
+      保存用户信息
+    </button>
+    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <uni-section title="设置" type="line">
+      <uni-list>
+        <uni-list-item title="去登陆" :to="`/pages/login/login`"> </uni-list-item>
+      </uni-list>
+    </uni-section>
+  </view>
+</template>
+
+<style lang="scss">
+//
+</style>
