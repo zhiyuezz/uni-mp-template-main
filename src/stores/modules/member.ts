556ddabffe8789c4ae53e9904c1e816c77fd35ec
zhiyuezz <3032149617@qq.com>
2023-11-17 22:02:47 +0800

    用户模块更新用户信息

--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -2,24 +2,30 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 // 定义 Store
-export const useMemberStore = defineStore('member', () => {
-  // 会员信息
-  const profile = ref<any>()
+export const useMemberStore = defineStore(
+  'member',
+  () => {
+    // 会员信息
+    const profile = ref<LoginResult>()
 
-  // 保存会员信息，登录时使用
-  const setProfile = (val: any) => {
-    profile.value = val
-  }
+    // 保存会员信息，登录时使用
+    const setProfile = (val: LoginResult) => {
+      profile.value = val
+    }
 
-  // 清理会员信息，退出时使用
-  const clearProfile = () => {
-    profile.value = undefined
-  }
+    // 清理会员信息，退出时使用
+    const clearProfile = () => {
+      profile.value = undefined
+    }
 
-  // 记得 return
-  return {
-    profile,
-    setProfile,
-    clearProfile
+    // 记得 return
+    return {
+      profile,
+      setProfile,
+      clearProfile
+    }
+  },
+  {
+    unistorage: true
   }
-})
+)
